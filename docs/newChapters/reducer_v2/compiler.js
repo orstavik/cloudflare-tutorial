81e6196bf47f5641d90961aa01e81e14ed90d302
intertext.no <orstavik77@gmail.com>
2021-02-08 18:32:19 +0100

    reducer v2

---
+++ docs/newChapters/reducer_v2/compiler.js
@@ -0,0 +1,83 @@
+// Parse parameters into either primitive arguments or <op><state> objects.
+function normalizeIdObserversMissingErrors(actions) {
+  return actions.map((a, i) => (a = [i, ...a], (a.length === 3 && a.push(`_observer_${i}`)), (a.length === 4 && a.push(`_error_${i}`)), a));
+}
+
+function parseParam(p) {
+  //primitives
+  if (p === '') return undefined;//todo what should we do here?
+  if (p[0] === '"') return p.substr(1, p.length - 2);
+  if (p[0] === "'") return p.substr(1, p.length - 2);
+  if (p === "null") return null;
+  if (p === "undefined") return undefined;
+  if (p === 'false') return false;
+  if (p === 'true') return true;
+  const num = parseFloat(p);
+  if (num === p) return num;
+
+  //<operator [*!&]{0,2}><state [a-z][a-zA-Z]*>
+  const [match, op, key] = p.match(/([*!&]{0,2})([a-z][a-zA-Z]*)/) || [];
+  if (match)
+    return {op, key};
+  throw new SyntaxError('Illegal parameter: ' + p);
+}
+
+const cache = {};
+
+function fromCacheFun(...keys) {
+  const key = JSON.stringify(keys.length === 1 ? keys[0] : keys);
+  if (!(key in cache))
+    throw new Error();
+  return cache[key];
+}
+
+function toCacheFun(val, ...keys) {
+  const key = JSON.stringify(keys.length === 1 ? keys[0] : keys);
+  cache[key] = val;
+}
+
+function a2a(a) {
+  return a;
+}
+
+//!...args, fun,!out, error
+//["out", ...args], fromCacheFun, out, _errorNoCache.
+// [...args, &_errorNoCache], fun, _toCache, error.
+// [&&out, "out", _toCache, !...args], toCacheFun.
+// _toCache, firstArgIsOutputFun, out.
+
+// First argument "out" is a string parameter with the name of the cache registry. Then non-! args are removed from the args list, and then the ! is removed from the argument name.
+// The full ...args list is added to the action, and the ! is removed from the argument names. An additional &_errorNoCache argument is added. The action is forced to wait for this argument, but it will not be included in the invocation.
+
+
+// caching observer. The first &&out parameter ensures that the action is cancelled as soon as the state out is set by somebody else. The &&out parameter is not included in the invocation. "out" is the string name for the cache registry. _toCache is the output of the fun that we want to cache. Then non-! args are removed from the args list, and then the ! is removed from the argument name.
+
+function convertCacheOperator([id, params, fun, output, error], get, put, operator, fun3) {
+  output = output.substr(operator.length);
+  const bangParams = params.filter(p => p.op === operator);
+  const newId = id + operator;
+  return [
+    [newId + 1, [`"${output}"`, ...bangParams], get, output, `_notFound${id}`],
+    [newId + 2, [...params, {op: '&', key: '_notFound' + id}], fun, `_toCache${id}`, error],
+    [newId + 3, [{op: '&&', key: output}, {op: '', key: '_toCache' + id}, `"${output}"`, ...bangParams],
+      put, `_observer_${newId}3`, `_error_${put.name}_${newId}3`],
+    [newId + 4, [{op: '', key: '_toCache' + id}], fun3, output, '_error_' + id + operator + 4]
+  ];
+}
+
+function compileCacheActions(actions) {
+  let res = [];
+  for (let action of actions) {
+    if (action[3][0] === '!')
+      res = res.concat(convertCacheOperator(action, fromCacheFun, toCacheFun, '!',  a2a));
+    else
+      res.push(action);
+  }
+  return res;
+}
+
+export function compile(actions){
+  actions = normalizeIdObserversMissingErrors(actions); //todo moved up init time
+  actions.forEach(action => action[1] = action[1].map(p => parseParam(p)));
+  return compileCacheActions(actions);
+}
---
+++ docs/newChapters/reducer_v2/mockRunTime.js
@@ -0,0 +1,40 @@
+import {rrListener} from "./ResponseRace.js";
+import {compile} from "./compiler.js";
+
+let previousState;
+//todo the checkMutations is now old. We can now implement the check mutations using the beforeInvoke and afterFrame methods.
+function checkMutations(msg) {
+  const nowObj = JSON.parse(atob(msg));
+  const i = nowObj.sequence.length;
+  const variables = nowObj.variables;
+  if (previousState) {
+    for (let [key, oldVal] of Object.entries(previousState)) {
+      const newVal = variables[key];
+      const oldString = JSON.stringify(oldVal);
+      const newString = JSON.stringify(newVal);
+      if (oldString !== newString)
+        window.parent.postMessage('!' + i + '!' + btoa(oldString) + '!' + btoa(newString), '*');
+    }
+  }
+  previousState = nowObj.variables;
+}
+
+function debug(msg) {
+  window.doMutationCheck && checkMutations(msg);
+  console.log(msg);
+  window.parent.postMessage(msg, '*');
+}
+
+const fetch = Object.assign(
+  new Event('fetch'), {
+    passThroughOnException: () => console.log('passThroughOnException.'),
+    waitUntil: async data => console.log('waitUntil: ', await data),
+    respondWith: async data => console.log('respondWith: ', await data),
+    request: {url: window.url}
+  });
+
+const cache = {cache3: {'"1"': {success: 'sunshiny day'}}};
+const compiledActions = compile(listOfActions);
+addEventListener('fetch', e => rrListener(compiledActions, e, debug, cache));
+
+dispatchEvent(fetch);
