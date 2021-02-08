3e2e4eae649b764db5a05156d57a6f19879fe656
intertext.no <orstavik77@gmail.com>
2021-02-08 23:16:11 +0100

    simplify state machine

--- docs/newChapters/reducer_v2/statemachine.js
+++ docs/newChapters/reducer_v2/statemachine.js
@@ -13,27 +13,12 @@ function runFun(fun, args) {
   }
 }
 
-//todo use remainingActions
-//todo maybe we should keep the frame.actions as a mutable list, and then drop the actions not in use. Instead of using the sequence as a check.
-//todo this is better. But. We get a different problem, we don't have the debug ready.. But. I need a different format for the debug.
 function firstReadyAction(frame) {
-  main: for (let action of frame.actions) {
-    const [id, params, fun, output] = action;
-    if (frame.sequence.indexOf(`:${id}_`) >= 0) continue;          //action already invoked, this can be done at the start
-
-    if (output in frame.variables) {               //goal completed, cancelling action
-      frame.sequence += `:${id}_c`;            // todo this and _waiting can be discovered from analysis of the actions and callSequence..
-      continue;
-    }
+  main: for (let action of frame.remainingActions) {
     const args = [];
-    for (let pp of params) {
+    for (let pp of action[1]) {
       if (!(pp instanceof Object)) {
         args.push(pp);
-      } else if (pp.op === '&&') {
-        if (pp.key in frame.variables) {
-          frame.sequence += `:${id}_c`;         //todo
-          continue main;                        //the && inverse required parameter has already been set, so this action can be cancelled
-        }
       } else if (pp.op === '&') {
         if (!(pp.key in frame.variables)) {
           // frame.sequence += `:${id}_w`; // todo illustrates when a function could have been called, but whose arguments was not ready.
@@ -46,6 +31,7 @@ function firstReadyAction(frame) {
         args.push(frame.variables[pp.key]);
       }
     }
+    frame.remainingActions.splice(frame.remainingActions.indexOf(action), 1);
     return {action, args};                                    //else, action is ready
   }
   return {};
@@ -61,6 +47,17 @@ function asyncActionReturns(frame, callTxt, key, val) {
 function setValue(frame, callTxt, key, val) {
   frame.sequence += callTxt;
   frame.variables[key] = val;
+  frame.remainingActions = frame.remainingActions.filter(([id, params, _, output]) => {
+    if (output === key || params.find(({op, key: p}) => op === '&&' && p === key)) {
+      frame.sequence += `:${id}_c`;
+      return false;
+    }
+    return true;
+  });
+  //todo Loose end analysis. "Loose ends" are states that are defined by the user, but not used as input anywhere.
+  // 'response' and _observer_s are such states, that will be run.
+  // There should be a rule that says that loose ends will be cleaned up and removed whenever possible.
+  // If you want to have a state that is to be left as a loose end, we need to give this a prefix.
   frame.postFrame?.call(null, frame);
 }
 
