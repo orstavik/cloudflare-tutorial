956f155640bec21c3bdd9c8526a2bae99268ef69
intertext.no <orstavik77@gmail.com>
2021-02-11 12:27:20 +0100

    updating compile function

--- docs/newChapters/reducer_v2/compiler.js
+++ docs/newChapters/reducer_v2/compiler.js
@@ -2,7 +2,6 @@
 // 1. syntax check for dead end states. We do this by checking each action. If the action is missing a required state (ie. a required state is neither an output of any other action, or a start variable), then we remove this action. We repeat this function recursively until there are no such actions removed in an entire pass). This will remove any loose ends. This can be done at compile time.
 //2. if this removes response, or any observers, then this will of course clear the way for any errors.
 
-
 // Parse parameters into either primitive arguments or <op><state> objects.
 function normalizeIdObserversMissingErrors(actions) {
   return actions.map((a, i) => (a = [i, ...a], (a.length === 3 && a.push(`_observer_${i}`)), (a.length === 4 && a.push(`_error_${i}`)), a));
@@ -27,75 +26,100 @@ function parseParam(p) {
   throw new SyntaxError('Illegal parameter: ' + p);
 }
 
-let cache = {};
+//EMPTY reserved word, and adding the
+const EMPTY = {};
 
-function fromCacheFun(...keys) {
-  const key = JSON.stringify(keys.length === 1 ? keys[0] : keys);
-  if (!(key in cache))
-    throw new Error();
-  return cache[key];
+const operators = {
+  '!': {
+    fun: function makeCache() {
+      return {
+        _cache: {},
+        _function: function cache(value, ...keys) {
+          const key = JSON.stringify(keys.length ? keys : keys[0]);
+          if (value === EMPTY) {
+            if (key in this._cache)
+              return this._cache[key] = value;
+            throw 'Expected Error, cache function is triggering alternative path.';
+          }
+          return this._cache[key] = value;
+        }
+      }._function
+    },
+    opName: 'bang',
+    funName: 'cache'
+  },
+  '?': {
+    fun: function makeGoldenMean() {
+      return {
+        _cache: {},
+        _function: function goldenMean(value, ...keys) {
+          const key = JSON.stringify(keys.length ? keys : keys[0]);
+          if (value === EMPTY)
+            throw 'Expected Error, goldenMean function is triggering alternative path.';
+          if(typeof value !== 'number' )
+            throw 'goldenMean only tackle number values.';
+          const nums = this._cache[key] || (this._cache[key] = []);
+          nums.push(value);
+          return nums.reduce((a, b) => (a + b)) / nums.length;
+        }
+      }._function
+    },
+    opName: 'goldenMean',
+    funName: 'goldenMean'
+  },
 }
 
-function toCacheFun(val, ...keys) {
-  const key = JSON.stringify(keys.length === 1 ? keys[0] : keys);
-  cache[key] = val;
-}
-
-function a2a(a) {
-  return a;
-}
-
-export function setCache(obj){
-  cache = obj;
-}
-
-//!...args, fun,!out, error
-//["out", ...args], fromCacheFun, out, _errorNoCache.
-// [...args, &_errorNoCache], fun, _toCache, error.
-// [&&out, "out", _toCache, !...args], toCacheFun.
-// _toCache, firstArgIsOutputFun, out.
-
-// First argument "out" is a string parameter with the name of the cache registry. Then non-! args are removed from the args list, and then the ! is removed from the argument name.
-// The full ...args list is added to the action, and the ! is removed from the argument names. An additional &_errorNoCache argument is added. The action is forced to wait for this argument, but it will not be included in the invocation.
-
-
-// caching observer. The first &&out parameter ensures that the action is cancelled as soon as the state out is set by somebody else. The &&out parameter is not included in the invocation. "out" is the string name for the cache registry. _toCache is the output of the fun that we want to cache. Then non-! args are removed from the args list, and then the ! is removed from the argument name.
-
-function convertCacheOperator([id, params, fun, output, error], get, put, operator, fun3) {
-  output = output.substr(operator.length);
-  const bangParams = params.filter(p => p.op === operator);
-  const newId = id + operator;
+// [!...params], fun,!out, error
+//
+//   becomes
+//                                                                                 //todo as it is right now, the fun cannot return undefined..
+// [empty, "out", ...bangparams], cache, out, _notInCacheID.                     //todo should I employ a separate 'empty' primitive?
+// [...params, &_notInCacheID], fun, _putInCacheID, error.
+// [_putInCacheID, "out", ...bangparams], cache, out, _errorPutInCacheID.
+//
+//bangparams = only arguments with bang in front, but with the bang removed
+//params = all arguments with bang removed.
+//_notInCacheID is `_notInCache${actionId}`
+//_putInCacheID is `_putInCache${actionId}`
+function convertCacheOperator([id, params, fun, output, error], OP, funName, opName) {
+  output = output.substr(OP.length);
+  const bangParams = params.filter(p => p.op === OP).map(({op, key}) => ({op: '', key}));
+  params = params.map(({op, key}) => ({op: op === OP ? '' : op, key})); //replace only the operator from the params
+  const _notInCacheID = `_notIn${opName + id}`;
+  const _putInCacheID = `_putIn${opName + id}`;
+  const _errorPutInCacheID = `_errorPutIn${id}`;
   return [
-    [newId + 1, [`"${output}"`, ...bangParams], get, output, `_notFound${id}`],
-    [newId + 2, [...params, {op: '&', key: '_notFound' + id}], fun, `_toCache${id}`, error],
-    [newId + 3, [{op: '&&', key: output}, {op: '', key: '_toCache' + id}, `"${output}"`, ...bangParams],
-      put, `_observer_${newId}3`, `_error_${put.name}_${newId}3`],
-    [newId + 4, [{op: '', key: '_toCache' + id}], fun3, output, '_error_' + id + operator + 4]
+    [id + OP + 1, [EMPTY, `"${output}"`, ...bangParams], funName, output, _notInCacheID],
+    [id + OP + 2, [...params, {op: '&', key: _notInCacheID}], fun, _putInCacheID, error],
+    [id + OP + 3, [{op: '', key: _putInCacheID}, `"${output}"`, ...bangParams], funName, output, _errorPutInCacheID]
   ];
 }
 
-function compileCacheActions(actions) {
+function compileCacheActions(actions, operators) {
   let res = [];
-  for (let action of actions) {
-    if (action[3][0] === '!')
-      res = res.concat(convertCacheOperator(action, fromCacheFun, toCacheFun, '!',  a2a));
-    else
-      res.push(action);
+  for (let OP in operators) {
+    const {funName, opName, fun} = operators[OP]; //todo here we have the fun, we could add that only if we need it.
+    for (let action of actions) {
+      if (action[3][0] === '!')
+        res = res.concat(convertCacheOperator(action, OP, funName, opName));
+      else
+        res.push(action);
+    }
   }
   return res;
 }
 
-export function compile(actions){
-  actions = normalizeIdObserversMissingErrors(actions); //todo moved up init time
+export function compile(actions) {
+  actions = normalizeIdObserversMissingErrors(actions);
   actions.forEach(action => action[1] = action[1].map(p => parseParam(p)));
-  return compileCacheActions(actions);
+  return compileCacheActions(actions, operators);
 }
 
 /**
  * BUILTIN FUNCTIONS
  */
-// doNothing:  a=>a
-// fail:       a=>throw a
+// first:  a => a
+// fail:   a => throw a
 export function get(obj, path) {
   if (path === '')
     return obj;
